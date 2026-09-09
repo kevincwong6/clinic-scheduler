70e85c13227508077bdfcffa50580fced79a3b26
Kevin Wong <48312245+kevincwong6@users.noreply.github.com>
2026-09-09 04:15:34 -0700

    Remove duplicate user greeting from footer

--- application/views/components/backend_footer.php
+++ application/views/components/backend_footer.php
@@ -1,10 +1,5 @@
 <?php
 $clinic_scheduler_version = '0.1.5';
-/**
- * Local variables.
- *
- * @var string $user_display_name
- */
 ?>
 <div id="footer" class="d-lg-flex justify-content-lg-start align-items-lg-center p-2 text-center text-lg-left mt-auto bg-body border-top" style="font-size: 13px;">
     <div class="mb-3 me-lg-5 mb-lg-0">
@@ -13,11 +8,5 @@ $clinic_scheduler_version = '0.1.5';
 
     <div class="mb-3 me-lg-5 mb-lg-0">
         <a href="<?= site_url('about') ?>">About / License</a>
-    </div>
-
-    <div class="ms-lg-auto">
-        <strong id="footer-user-display-name">
-            <?= lang('hello') . ', ' . e($user_display_name) ?>!
-        </strong>
     </div>
 </div>
